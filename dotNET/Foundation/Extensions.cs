82bf8fd7493dc0d0cfdc697917fde40ffd6950b6
Dávid Molnár <xmolna02@stud.fit.vutbr.cz>
2017-06-04 13:48:22 +0200

    add .NET extensions

---
+++ dotNET/Foundation/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Foundation
+{
+    public static class Extensions
+    {
+        public static bool In<T>(this T source, params T[] list)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            return list.Contains(source);
+        }
+    }
+}
