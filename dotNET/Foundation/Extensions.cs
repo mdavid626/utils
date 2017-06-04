b08ef28f77f63f09f382f4c3d559312b65a5ba27
Dávid Molnár <xmolna02@stud.fit.vutbr.cz>
2017-06-04 13:58:35 +0200

    add new extension methods

--- dotNET/Foundation/Extensions.cs
+++ dotNET/Foundation/Extensions.cs
@@ -13,5 +13,37 @@ namespace Foundation
                 throw new ArgumentNullException(nameof(source));
             return list.Contains(source);
         }
+
+        public static string Format(this string s, params object[] args)
+        {
+            return string.Format(s, args);
+        }
+
+        public static bool CoinToss(this Random rng)
+        {
+            return rng.Next(2) == 0;
+        }
+
+        public static T OneOf<T>(this Random rng, params T[] things)
+        {
+            return things[rng.Next(things.Length)];
+        }
+
+        public static bool Between<T>(this T actual, T lower, T upper) where T : IComparable<T>
+        {
+            return actual.CompareTo(lower) >= 0 && actual.CompareTo(upper) < 0;
+        }
+
+        public static void AddRange<T, S>(this ICollection<T> list, params S[] values)
+            where S : T
+        {
+            list.AddRange(values);
+        }
+
+        public static void Apply<T>(this IEnumerable<T> @enum, Action<T> action)
+        {
+            foreach (var item in @enum)
+                action(item);
+        }
     }
 }
